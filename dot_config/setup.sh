88db1e6b63efdc40a15039cba480c4ccb118c8ec
e-radu <e-radu@radu>
2024-09-29 19:59:56 +0200

    more tweaks on setup script

--- dot_config/setup.sh
+++ dot_config/setup.sh
@@ -1,26 +1,35 @@
 sudo apt-get install nala -y
 sudo nala update
 sudo nala upgrade -y
-sudo nala install git rcm build-essential checkinstall zlib1g-dev libssl-dev wget gpg unzip -y
+sudo nala install git tree build-essential checkinstall zlib1g-dev libssl-dev
+sudo nala install wget gpg unzip gcc make ripgrep xclip -y
 sudo nala install cmake -y
+
+echo "Installing neovim"
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+echo "Installing neovim plugins"
+git clone https://github.com/e-radu/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
 if [ ! -d "~/.rustup"]; then
 echo "Installing Rust Cargo"
 curl https://sh.rustup.rs -sSf | sh
-echo ". \"$HOME/.cargo/env\"" >> ~/.config/.zshrc
+else
+echo "Rust Cargo already installed"
 fi
-
-echo "Reloading ZSH config"
-source ~/.config/.zshrc
 
 starship -V
 if [ ! $? -eq 0 ]; then
 echo "Installing Starship"
 curl -sS https://starship.rs/install.sh | sh
-echo "#eval \"$(starship init zsh)\"" >> ~/.config/.zshrc
 else
 echo "Starship already installed"
 fi
+
+echo "Reloading ZSH config"
+source ~/.config/.zshrc
 
 echo "Installing eza"
 cargo install --locked eza
@@ -32,7 +41,8 @@ echo "Installing bat"
 cargo install --locked bat
 
 echo "Installing FZF"
-sudo nala install fzf -y
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 echo "Installing fd-find"
 sudo nala install fd-find -y
