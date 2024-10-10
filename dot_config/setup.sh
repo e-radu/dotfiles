6207acc88c8153372494cb3d6476af5da6d78909
e-radu <ene.m.radu@gmail.com>
2024-10-10 13:10:30 +0200

    added wezterm and brave browser install

--- dot_config/setup.sh
+++ dot_config/setup.sh
@@ -6,6 +6,28 @@ sudo nala install git tree build-essential checkinstall zlib1g-dev libssl-dev -y
 sudo nala install wget gpg unzip gcc make ripgrep xclip fd-find tmux -y
 # sudo nala install cmake -y
 
+wezterm --version
+if [ ! $? -eq 0 ]; then
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo nala update
+sudo nala install wezterm -y
+else
+echo "Wezterm already installed"
+fi
+
+brave-browser --version
+if [ ! $? -eq 0 ]; then
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo nala update
+sudo nala install brave-browser -y
+else
+echo "Brave browser already installed"
+fi
+
+
+
 function check_and_install_tool {
 $1 --version
 if [ ! $? -eq 0 ]; then
