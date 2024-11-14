ccf259b1cfa6df5b3ec9241bc3acdb5e429d093b
e-radu <ene.m.radu@gmail.com>
2024-11-14 15:00:14 +0100

    updated from kickstart.nvim to lazy.nvim

--- dot_config/setup.sh
+++ dot_config/setup.sh
@@ -10,98 +10,96 @@ fc-cache -f -v
 sudo nala install ffmpegthumbnailer jq poppler-utils fd-find ripgrep xclip -y
 wezterm --version
 if [ ! $? -eq 0 ]; then
-curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
-echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
-sudo nala update
-sudo nala install wezterm -y
+    curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+    echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+    sudo nala update
+    sudo nala install wezterm -y
 else
-echo "Wezterm already installed"
+    echo "Wezterm already installed"
 fi
 
 brave-browser --version
 if [ ! $? -eq 0 ]; then
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo nala update
-sudo nala install brave-browser -y
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo nala update
+    sudo nala install brave-browser -y
 else
-echo "Brave browser already installed"
+    echo "Brave browser already installed"
 fi
-
-
 
 function check_and_install_tool {
-$1 --version
-if [ ! $? -eq 0 ]; then
-echo "Installing $1 ..."
-cargo install --locked $1
-else
-echo "--> $1 already installed."
-fi
+    $1 --version
+    if [ ! $? -eq 0 ]; then
+        echo "Installing $1 ..."
+        cargo install --locked $1
+    else
+        echo "--> $1 already installed."
+    fi
 }
 
 nvim --version
 if [ ! $? -eq 0 ]; then
-echo "Installing neovim"
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+    echo "Installing neovim"
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+    sudo rm -rf /opt/nvim
+    sudo tar -C /opt -xzf nvim-linux64.tar.gz
+    rm nvim-linux64.tar.gz
 
-echo "Installing neovim plugins"
-git clone https://github.com/e-radu/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+    echo "Installing neovim plugins"
+    git clone https://github.com/e-radu/lazy.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 fi
 
 cargo --version
-if  [ ! $? -eq 0 ]; then
-echo "Installing Rust Cargo"
-curl https://sh.rustup.rs -sSf | sh
+if [ ! $? -eq 0 ]; then
+    echo "Installing Rust Cargo"
+    curl https://sh.rustup.rs -sSf | sh
 else
-echo "Rust Cargo already installed"
+    echo "Rust Cargo already installed"
 fi
 
 starship -V
 if [ ! $? -eq 0 ]; then
-echo "Installing Starship"
-curl -sS https://starship.rs/install.sh | sh
+    echo "Installing Starship"
+    curl -sS https://starship.rs/install.sh | sh
 else
-echo "Starship already installed"
+    echo "Starship already installed"
 fi
 
 lazygit --version
 if [ ! $? -eq 0 ]; then
-echo "Installing lazygit"
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-rm lazygit.tar.gz
-rm lazygit
+    echo "Installing lazygit"
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+    rm lazygit.tar.gz
+    rm lazygit
 else
-echo "--> lazygit already installed"
+    echo "--> lazygit already installed"
 fi
 
 cargoTools=("eza" "bat" "zoxide" "zellij" "yazi-fm" "yazi-cli")
 
 for tool in "${cargoTools[@]}"; do
-check_and_install_tool $tool
+    check_and_install_tool $tool
 done
 
 delta --version
 if [ ! $? -eq 0 ]; then
-echo "Installing delta"
-cargo install --locked git-delta
+    echo "Installing delta"
+    cargo install --locked git-delta
 else
-echo "--> delta already installed"
+    echo "--> delta already installed"
 fi
 
 fzf --version
 if [ ! $? -eq 0 ]; then
-echo "Installing FZF"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+    echo "Installing FZF"
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
 else
-echo "--> fzf already installed"
+    echo "--> fzf already installed"
 fi
 
 chsh -s $(which zsh)
@@ -109,4 +107,3 @@ zsh
 
 echo "Reloading ZSH config"
 source ~/.zshrc
-
