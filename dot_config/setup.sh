6bb0adbe8bb0d85d9f8f88f5c6c7639a42296889
e-radu <ene.m.radu@gmail.com>
2024-12-09 18:16:39 +0100

    added nodejs - dependency for nvim copilot

--- dot_config/setup.sh
+++ dot_config/setup.sh
@@ -94,6 +94,16 @@ zsh
 echo "Reloading ZSH config"
 source ~/.zshrc
 
+node -v
+if [ ! $? -eq 0 ]; then
+    echo "Installing NodeJS"
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+    source ~/.zshrc
+    nvm install 22
+else
+    echo "NodeJS already installed"
+fi
+
 cargoTools=("eza" "bat" "zoxide" "yazi-fm" "yazi-cli" "tlrc")
 
 for tool in "${cargoTools[@]}"; do
