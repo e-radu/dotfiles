7aa09a5027923ef56acae1f6831445c3f91bf26c
e-radu <ene.m.radu@gmail.com>
2025-05-07 11:45:44 +0200

    added gh cli and zsh integration

--- dot_config/setup.sh
+++ dot_config/setup.sh
@@ -81,6 +81,24 @@ else
     echo "--> fzf already installed"
 fi
 
+gh --version
+if [ ! $? -eq 0 ]; then
+    echo "Installing GitHub CLI"
+    (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) &&
+        sudo mkdir -p -m 755 /etc/apt/keyrings &&
+        out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg &&
+        cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
+        sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+        sudo nala update &&
+        sudo nala install gh -y
+
+    gh auth login --web
+    gh extension install github/gh-copilot --force
+else
+    echo "--> GitHub CLI already installed"
+fi
+
 chsh -s $(which zsh)
 zsh
 
