4decc45b6bfbabd7c803d9368c4b4144562683c6
e-radu <ene.m.radu@gmail.com>
2025-06-05 16:14:12 +0200

    added missing utils: curl, ffmpeg

--- dot_config/setup.sh
+++ dot_config/setup.sh
@@ -3,13 +3,13 @@ sudo apt-get install nala -y
 sudo nala upgrade -y
 sudo nala install zsh -y
 sudo nala install git tree build-essential checkinstall zlib1g-dev libssl-dev -y
-sudo nala install wget gpg unzip gcc make -y
+sudo nala install wget curl gpg unzip gcc make -y
 # install dependencies for tmux
 sudo nala install yacc libncurses5-dev libncursesw5-dev libevent-dev -y
 # load custom fonts
 fc-cache -f -v
 # install dependencies for yazi
-sudo nala install ffmpegthumbnailer jq poppler-utils fd-find ripgrep xclip -y
+sudo nala install ffmpeg ffmpegthumbnailer jq poppler-utils fd-find ripgrep xclip -y
 sudo ln --symbolic $(which fdfind) /usr/local/bin/fd
 
 # Remove previous tmux installation
@@ -19,9 +19,9 @@ if [ ! $? -eq 0 ]; then
     current_path=$(pwd)
     mkdir -p ~/tmux_temp
     cd ~/tmux_temp
-    wget https://github.com/tmux/tmux/releases/download/3.5/tmux-3.5.tar.gz
-    tar -zxf tmux-3.5.tar.gz
-    cd tmux-3.5
+    wget https://github.com/tmux/tmux/releases/download/3.5a/tmux-3.5a.tar.gz
+    tar -zxf tmux-3.5a.tar.gz
+    cd tmux-3.5a
     ./configure && make
     sudo make install
     cd $current_path
@@ -34,10 +34,10 @@ fi
 nvim --version
 if [ ! $? -eq 0 ]; then
     echo "Installing neovim"
-    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
     sudo rm -rf /opt/nvim
-    sudo tar -C /opt -xzf nvim-linux64.tar.gz
-    rm nvim-linux64.tar.gz
+    sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+    rm nvim-linux-x86_64.tar.gz
 
     echo "Installing neovim plugins"
     git clone https://github.com/e-radu/lazy.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
@@ -63,11 +63,12 @@ lazygit --version
 if [ ! $? -eq 0 ]; then
     echo "Installing lazygit"
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    echo "Latest lazygit version: $LAZYGIT_VERSION"
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
     sudo install lazygit /usr/local/bin
     rm lazygit.tar.gz
-    rm lazygit
+    rm -rf lazygit
 else
     echo "--> lazygit already installed"
 fi
